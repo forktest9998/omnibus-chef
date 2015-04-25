59562f00d31ebc094ea3cf17af49041befb19d16
Bao Nguyen <bao@fastly.com>
2015-04-24 17:21:07 -0700

    correcting config value for s3 publish

--- omnibus.rb
+++ omnibus.rb
@@ -35,8 +35,8 @@ s3_access_key  ENV['AWS_ACCESS_KEY_ID']
 s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
 s3_bucket      'opscode-omnibus-cache'
 
-config.publish_s3_access_key("#{ENV['AWS_ACCESS_KEY_ID']}")
-config.publish_s3_secret_key("#{ENV['AWS_SECRET_ACCESS_KEY']}")
+publish_s3_access_key ENV['AWS_ACCESS_KEY_ID']
+publish_s3_secret_key ENV['AWS_SECRET_ACCESS_KEY']
 
 build_retries 3
 fetcher_retries 3
