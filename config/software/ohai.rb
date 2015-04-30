8f8b9cc20752f4f3fb8b6eed26d2455c0a17839c
Bao Nguyen <bao@fastly.com>
2015-04-30 00:51:46 -0700

    fastlys ohai, to get only the default route to ohai

---
+++ config/software/ohai.rb
@@ -0,0 +1,29 @@
+#
+# Copyright:: Copyright (c) 2015 - Fastly Inc.
+
+name "ohai"
+default_version "6.16.0"
+
+source git: "git@github.com:fastly/ohai.git"
+
+relative_path "ohai"
+
+if windows?
+  dependency "ruby-windows"
+  dependency "ruby-windows-devkit"
+else
+  dependency "ruby"
+  dependency "rubygems"
+end
+
+dependency "bundler"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  bundle "install --without development", env: env
+
+  gem "build ohai.gemspec", env: env
+  gem "install ohai*.gem" \
+      " --no-ri --no-rdoc", env: env
+end
