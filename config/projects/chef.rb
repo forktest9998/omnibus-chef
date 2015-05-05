a5b4dbe36543447f73bcf462b3184edc6da99f71
Phil Cohen <pcohen@fastly.com>
2015-05-04 19:46:03 -0700

    update Chef to v11.14.6, bump dependencies

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -50,8 +50,8 @@ end
 # Software does).
 override :cacerts, version: '2014.08.20'
 
-override :bundler,        version: "1.7.2"
-override :ruby,           version: "2.1.4"
+override :bundler,        version: "1.7.15"
+override :ruby,           version: "2.1.6"
 ######
 # Ruby 2.1.3 is currently not working on windows due to:
 # https://github.com/ffi/ffi/issues/375
@@ -60,8 +60,8 @@ override :ruby,           version: "2.1.4"
 # override :'ruby-windows-devkit', version: "4.7.2-20130224-1151"
 override :'ruby-windows', version: "2.0.0-p451"
 ######
-override :rubygems,       version: "2.4.4"
-override :chef,       version: "11.8.0"
+override :rubygems,       version: "2.4.5"
+override :chef,       version: "11.14.6"
 
 dependency "preparation"
 dependency "chef"
