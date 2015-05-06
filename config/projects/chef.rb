f8031c911390bb2fe4dc0d51ff6df6a5518f3bd6
Phil Cohen <pcohen@fastly.com>
2015-05-05 20:13:59 -0700

    bump `chef` version up to 11.18.2

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -61,7 +61,7 @@ override :ruby,           version: "2.1.6"
 override :'ruby-windows', version: "2.0.0-p451"
 ######
 override :rubygems,       version: "2.4.6"
-override :chef,       version: "11.14.6"
+override :chef,       version: "11.18.12"
 
 dependency "preparation"
 dependency "chef"
